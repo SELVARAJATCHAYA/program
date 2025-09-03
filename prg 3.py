6fad67bf896360e37b6ac479c85554d30aa267ae
ATCHAYA SELVARAJ <atchayasathya33@gmail.com>
2025-09-03 15:26:29 +0530

    Add files via upload

---
+++ prg 3.py
@@ -0,0 +1,39 @@
+my_set={1,2,3,4}
+print("Initially,my set is",my_set)
+my_list=[4,5,6,7]
+my_set=set(my_list)
+print("my_set=set(my_list)",my_set)
+my_set.add(9)
+print("my_set.add(9):",my_set)
+my_set.update([6,7])
+print("my_set.update([6,7]):",my_set)
+my_set.remove(4)
+print("my_set.remove(4):",my_set)
+my_set.discard(5)
+print("my_set.discard(5):",my_set)
+print(2 in my_set)
+print(8 in my_set)
+set1={1,2,3,4}
+print("set1={1,2,3,4}")
+set2={3,4,5}
+print("set2={3,4,5}")
+union_set1=set1.union(set2)
+print(union_set1)
+intersection_set1=set1.intersection(set2)
+print(intersection_set1)
+difference_set1=set1.difference(set2)
+print(difference_set1)
+set1.update(set2)
+print(set1)
+set1.intersection_update(set2)
+print(set1)
+set1.difference_update(set2)
+print(set2)
+set1={1,2,3,4}
+set2={3,4,5}
+symmetric_difference=set1.symmetric_difference(set2)
+print(symmetric_difference)
+set1={1,2,3}
+set2={3,4,5}
+print(set2.issubset(set1))
+print(set2.issubset(set2))
